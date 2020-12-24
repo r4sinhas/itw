0d2a1128bff50455868f156f0e767f958415c125
r4sinhas <76540362+r4sinhas@users.noreply.github.com>
2020-12-24 19:07:45 +0000

    Add files via upload

---
+++ java.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+   $("#menu-toggle").click(function(e){
+     e.preventDefault();
+     $("#wrapper").toggleClass("menuDisplayed");
+   });
+ });
+ 
+
+ function searchBar() {
+   console.log("it works"); /* working on click */
+ }
